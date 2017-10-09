dcbc5d9f965fd9659dd7e8ce659ff8fc83895515
maraalexa <maraalexa57@gmail.com>
2017-10-09 17:57:32 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <input type="text" v-on:input="cssClass = $event.target.value" >
+    <p v-bind:class="cssClass">Class: {{ cssClass }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      cssClass: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+
+  p {
+    background: grey;
+  }
+
+  .red {
+    background: red;
+    padding: 1rem;
+  }
+  .blue {
+    background: blue;
+    padding: 1rem;
+  }
+
+}
+</style>
